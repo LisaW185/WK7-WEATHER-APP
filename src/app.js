9c128d3a3c809120108fa1799dae23cb252cdb0a
LisaW185 <lisaw0518@gmail.com>
2023-09-02 19:24:12 +0100

    Showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
+let apiKey = "d9838tb6c0b5af99683b9fo2a4b11ac7";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Cardiff&key=${apiKey}&units=metric`;
 
+console.log(apiUrl);
+function displayTemperature(response) {
+  console.log(response.data);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+axios.get(apiUrl).then(displayTemperature);
