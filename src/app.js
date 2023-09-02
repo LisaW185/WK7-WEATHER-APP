2607917fdaaf3037cb82e7fd07a3ca06330a3258
LisaW185 <lisaw0518@gmail.com>
2023-09-02 19:54:07 +0100

    Update the time

--- src/app.js
+++ src/app.js
@@ -2,10 +2,36 @@ let apiKey = "d9838tb6c0b5af99683b9fo2a4b11ac7";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Cardiff&key=${apiKey}&units=metric`;
 
 console.log(apiUrl);
+
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
   let descriptionElement = document.querySelector("#description");
   descriptionElement.innerHTML = response.data.condition.description;
   let temperatureElement = document.querySelector("#temperature");
