42bbd482f8a059c42b80cb46656d441b2067e3d3
LisaW185 <lisaw0518@gmail.com>
2023-09-11 23:03:18 +0100

    Created JS Forecast Template

--- src/app.js
+++ src/app.js
@@ -71,6 +71,34 @@ function displayCelsiusTemperature(event) {
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+              <div class="col-2">
+                <div class="weather-forecast-date">${day}</div>
+                <img
+                  src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
+                  alt=""
+                  width="50"
+                />
+                <div class="weather-forecast-temperatures">
+                  <span class="weather-forecast-temperature-max">27°</span>
+                  <span class="weather-forecast-temperature-min">15°</span>
+                </div>
+              </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 //GLOBAL VARIABLES//
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
@@ -84,3 +112,4 @@ celsiusLink.addEventListener("click", displayCelsiusTemperature);
 let celsiusTemperature = null;
 
 search("Cardiff");
+displayForecast();
